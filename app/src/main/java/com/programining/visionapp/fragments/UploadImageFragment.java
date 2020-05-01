01b295e2efc8b03a7af7df120edfd268c8b04ba7
Yousuf <josephali1993@gmail.com>
2020-05-01 13:18:38 +0400

    +UploadImageFragment : fragment which will handel uploading image to vision and receive the response.
    #UploadImageFragment : Extract colors and string values.
    
    + build.gradle(Module:App) : + implementation 'androidx.legacy:legacy-support-v4:1.0.0'

---
+++ app/src/main/java/com/programining/visionapp/fragments/UploadImageFragment.java
@@ -0,0 +1,34 @@
+package com.programining.visionapp.fragments;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.programining.visionapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class UploadImageFragment extends Fragment {
+
+
+    public UploadImageFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View parentView = inflater.inflate(R.layout.fragment_upload_image, container, false);
+
+
+        return parentView;
+    }
+
+}
